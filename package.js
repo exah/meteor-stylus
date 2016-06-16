e5e77b386297b7174fc8fefa68d61ca2ba9108f5
John Grishin <r@exah.ru>
2016-06-16 13:43:09 +0300

    Bump version: 2.0.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "Expressive, dynamic, robust CSS",
   name: "exah:stylus",
-  version: "2.0.0",
+  version: "2.0.1",
   git: "https://github.com/exah/meteor-stylus.git"
 });
 
